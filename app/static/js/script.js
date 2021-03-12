96ef7fa1635d158fe2b5bd4d8de16181a161e173
vadger <vadger@yahoo.com>
2021-03-12 11:27:21 +0200

    refactor code

---
+++ app/static/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const closePopup = document.querySelector('.popup__close');
+    const backdropClose = document.querySelector('.backdrop');
+    const buttonOpenPopup = document.querySelector('.open-popup');
+
+    buttonOpenPopup.addEventListener("click", function () {
+        backdropClose.classList.remove("is-hidden");
+        document.querySelector(".popup").classList.remove("is-hidden");
+        document.body.classList.add("body__hidden");
+    });
+
+    closePopup.addEventListener("click", function () {
+        backdropClose.classList.add("is-hidden");
+        document.querySelector(".popup").classList.add("is-hidden");
+        document.body.classList.remove("body__hidden");
+
+    });
+    backdropClose.addEventListener("click", function () {
+        backdropClose.classList.add("is-hidden");
+        document.querySelector(".popup").classList.add("is-hidden");
+        document.body.classList.remove("body__hidden");
+        })
+
+})
